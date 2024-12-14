2d56f1a0deae4063022f0c13b1e2c7112616129e
Alexander Chistov <aleksandrchistov.ru@yandex.ru>
2024-12-14 18:55:59 +0700

    add tests for class Managers

---
+++ test/ru/yandex/practicum/task/ManagersTest.java
@@ -0,0 +1,20 @@
+package ru.yandex.practicum.task;
+
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.task.interfaces.HistoryManager;
+import ru.yandex.practicum.task.interfaces.TaskManager;
+
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+
+class ManagersTest {
+
+    @Test
+    void getDefault() {
+        assertInstanceOf(TaskManager.class, Managers.getDefault());
+    }
+
+    @Test
+    void getDefaultHistory() {
+        assertInstanceOf(HistoryManager.class, Managers.getDefaultHistory());
+    }
+}
