14f768fb74d7e0035ae2ee96e42a5faabe9d0019
Alexander Chistov <aleksandrchistov.ru@yandex.ru>
2025-01-26 18:05:40 +0700

    add remove and checkDouble tests for InMemoryHistoryManager

--- test/ru/yandex/practicum/task/InMemoryHistoryManagerTest.java
+++ test/ru/yandex/practicum/task/InMemoryHistoryManagerTest.java
@@ -24,11 +24,45 @@ class InMemoryHistoryManagerTest {
         Epic epic = new Epic("Epic", "Some epic", TaskStatus.NEW);
         Subtask subtask = new Subtask("Subtask", "Some subtask", TaskStatus.NEW, 1);
 
+        task.setId(1);
+        epic.setId(2);
+        subtask.setId(3);
+
         historyManager.add(task);
         historyManager.add(epic);
         historyManager.add(subtask);
 
-        assertEquals(3, historyManager.getHistory().size());
+        assertEquals(3, historyManager.getHistory().size(), "Кол-во добавленных задач не совпадает");
+    }
+
+    @Test
+    void remove() {
+        Task task = new Task("Task", "Some task", TaskStatus.NEW);
+
+        task.setId(1);
+
+        historyManager.add(task);
+
+        assertEquals(1, historyManager.getHistory().size(), "Кол-во добавленных задач не совпадает");
+
+        historyManager.remove(task.getId());
+
+        assertEquals(0, historyManager.getHistory().size(), "Кол-во задач после удаления не совпадает");
+    }
+
+    @Test
+    void checkDouble() {
+        Task task = new Task("Task", "Some task", TaskStatus.NEW);
+
+        task.setId(1);
+
+        historyManager.add(task);
+
+        assertEquals(1, historyManager.getHistory().size(), "Кол-во добавленных задач не совпадает");
+
+        historyManager.add(task);
+
+        assertEquals(1, historyManager.getHistory().size(), "Одна задача не должна быть добавлена дважды");
     }
 
 }
