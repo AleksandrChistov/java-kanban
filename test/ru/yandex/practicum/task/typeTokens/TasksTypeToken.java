290a626733753f129d20ba9629e79e7b0fc20873
Alexander Chistov <aleksandrchistov.ru@yandex.ru>
2025-03-03 17:38:40 +0700

    add type tokens

---
+++ test/ru/yandex/practicum/task/typeTokens/TasksTypeToken.java
@@ -0,0 +1,8 @@
+package ru.yandex.practicum.task.typeTokens;
+
+import com.google.gson.reflect.TypeToken;
+import ru.yandex.practicum.task.tasks.Task;
+
+import java.util.List;
+
+public class TasksTypeToken extends TypeToken<List<Task>> {}
